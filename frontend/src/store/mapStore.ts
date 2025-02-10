7c4b0d1ab3275f4b30b433676b679684136b34f0
PRAVIN KSHIRSAGAR <107901482+Pravin223047@users.noreply.github.com>
2025-02-10 22:24:45 +0530

    Update mapStore.ts

--- frontend/src/store/mapStore.ts
+++ frontend/src/store/mapStore.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const API_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api/auth"
-    : "/api/event";
+    ? "http://localhost:5000/api/map"
+    : "/api/map";
 
 axios.defaults.withCredentials = true;
 
